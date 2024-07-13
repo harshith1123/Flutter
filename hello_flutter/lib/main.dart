03e861e37698e0784e27867516c1829f501d80b7
harshith1123 <somethingincredible123@gmail.com>
2024-07-13 15:30:33 +0530

    initial setup

---
+++ hello_flutter/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:Scaffold(
+        backgroundColor:Colors.blueGrey ,
+        appBar: AppBar(
+          title: const Text('Im excited to learn flutter'),
+        ),)
+    )
+  );
+}
+
